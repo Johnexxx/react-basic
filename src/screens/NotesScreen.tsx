899e576a47d15283503ec2a32797e3341ba24f74
alttos-jcd <johnexxx15@gmail.com>
2025-07-15 15:10:21 +0800

    note

--- src/screens/NotesScreen.tsx
+++ src/screens/NotesScreen.tsx
@@ -183,6 +183,7 @@ const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#f2f2f2', padding: 16 },
   header: {
     fontSize: 22,
+    
     fontWeight: '700',
     textAlign: 'center',
     marginBottom: 16,
